bf0f33388024644e2b31b63e50037dc8274da73e
ZakkeryDaRebel <twminebro@gmail.com>
2024-05-07 00:02:03 -0600

    ChessBoard, ChessPiece, and ChessPosition tests have all been passed.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -52,29 +52,29 @@ public class ChessBoard {
         ChessPiece.PieceType typePawn = ChessPiece.PieceType.PAWN;
 
 
-        addPiece( new ChessPosition(0,0), new ChessPiece(teamWhite, typeRook));
-        addPiece( new ChessPosition(0,1), new ChessPiece(teamWhite, typeKnight));
-        addPiece( new ChessPosition(0,2), new ChessPiece(teamWhite, typeBishop));
-        addPiece( new ChessPosition(0,3), new ChessPiece(teamWhite, typeQueen));
-        addPiece( new ChessPosition(0,4), new ChessPiece(teamWhite, typeKing));
-        addPiece( new ChessPosition(0,5), new ChessPiece(teamWhite, typeBishop));
-        addPiece( new ChessPosition(0,6), new ChessPiece(teamWhite, typeKnight));
-        addPiece( new ChessPosition(0,7), new ChessPiece(teamWhite, typeRook));
+        addPiece( new ChessPosition(1,1), new ChessPiece(teamWhite, typeRook));
+        addPiece( new ChessPosition(1,2), new ChessPiece(teamWhite, typeKnight));
+        addPiece( new ChessPosition(1,3), new ChessPiece(teamWhite, typeBishop));
+        addPiece( new ChessPosition(1,4), new ChessPiece(teamWhite, typeQueen));
+        addPiece( new ChessPosition(1,5), new ChessPiece(teamWhite, typeKing));
+        addPiece( new ChessPosition(1,6), new ChessPiece(teamWhite, typeBishop));
+        addPiece( new ChessPosition(1,7), new ChessPiece(teamWhite, typeKnight));
+        addPiece( new ChessPosition(1,8), new ChessPiece(teamWhite, typeRook));
 
-        for(int i = 0; i <= 7; i++) {
-            addPiece( new ChessPosition(1,i), new ChessPiece(teamWhite, typePawn));
+        for(int i = 1; i <= 8; i++) {
+            addPiece( new ChessPosition(2,i), new ChessPiece(teamWhite, typePawn));
         }
 
-        addPiece( new ChessPosition(8,0), new ChessPiece(teamBlack, typeRook));
-        addPiece( new ChessPosition(8,1), new ChessPiece(teamBlack, typeKnight));
-        addPiece( new ChessPosition(8,2), new ChessPiece(teamBlack, typeBishop));
-        addPiece( new ChessPosition(8,3), new ChessPiece(teamBlack, typeQueen));
-        addPiece( new ChessPosition(8,4), new ChessPiece(teamBlack, typeKing));
-        addPiece( new ChessPosition(8,5), new ChessPiece(teamBlack, typeBishop));
-        addPiece( new ChessPosition(8,6), new ChessPiece(teamBlack, typeKnight));
-        addPiece( new ChessPosition(8,7), new ChessPiece(teamBlack, typeRook));
+        addPiece( new ChessPosition(8,1), new ChessPiece(teamBlack, typeRook));
+        addPiece( new ChessPosition(8,2), new ChessPiece(teamBlack, typeKnight));
+        addPiece( new ChessPosition(8,3), new ChessPiece(teamBlack, typeBishop));
+        addPiece( new ChessPosition(8,4), new ChessPiece(teamBlack, typeQueen));
+        addPiece( new ChessPosition(8,5), new ChessPiece(teamBlack, typeKing));
+        addPiece( new ChessPosition(8,6), new ChessPiece(teamBlack, typeBishop));
+        addPiece( new ChessPosition(8,7), new ChessPiece(teamBlack, typeKnight));
+        addPiece( new ChessPosition(8,8), new ChessPiece(teamBlack, typeRook));
 
-        for(int i = 0; i <= 7; i++) {
+        for(int i = 1; i <= 8; i++) {
             addPiece( new ChessPosition(7,i), new ChessPiece(teamBlack, typePawn));
         }
     }
